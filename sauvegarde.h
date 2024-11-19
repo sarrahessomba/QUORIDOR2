6aa6b8c9ce09e4119b1ff32b3cb1ec93f7a51480
Jeden971 <jeden.siloalexandrealexis@edu.ece.fr>
2024-11-19 09:31:51 +0100

    Update sauvegarde.h

--- sauvegarde.h
+++ sauvegarde.h
@@ -1,5 +1,6 @@
 #ifndef SAUVEGARDE_H
 #define SAUVEGARDE_H
+//initialisation du sous prog permettant de sauvegarder less éléments du jeux 1 à1
 void sauvegarder_partie(int tableau[17][17], int joueurs_x[4], int joueurs_y[4], int N, int joueur_actuel);
 
 #endif //SAUVEGARDE_H
