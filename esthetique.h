45400207b7e125c843e9abaf28ee37167ce9ec69
Jeden971 <jeden.siloalexandrealexis@edu.ece.fr>
2024-11-19 09:16:35 +0100

    Create esthetique.h
    def les fonctions esthétiques

---
+++ esthetique.h
@@ -0,0 +1,16 @@
+#define ESTHETIQUE_H
+#ifndef AFFICHAGE_H
+#define AFFICHAGE_H
+#define CASE_VIDE '·'
+#define BARRIERE '|'
+#define BARRIERE_H'-'
+#define PION_J1 '⬤'     // Pion du joueur 1
+#define PION_J2 '○'     // Pion du joueur 2
+#define PION_J3 '◉'     // Pion du joueur 3 (seulement si 4 joueurs)
+#define PION_J4 '◎'     // Pion du joueur 4 (seulement si 4 joueurs)
+void esthetique(void);
+void esthetique2 (int i);
+
+
+#endif ESTHETIQUE_H
+#endif ESTHETIQUE_H
