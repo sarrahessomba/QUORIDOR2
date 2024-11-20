bcecda6f7bb7981769285877e85407e2c2167f48
sarrahessomba <sarrahessomba8@gmail.com>
2024-11-20 16:14:11 +0100

    Create new plateau.h
    fonctions lies au plateau

---
+++ new plateau.h
@@ -0,0 +1,7 @@
+
+#include "fonction.joueur.h"
+#ifndef PLATEAU_H
+#define PLATEAU_H
+void init_tab (int tab[17][17],int N,joueur joueur1,joueur joueur2,  joueur joueur3,joueur joueur4);
+void affichage_plateau(int tab[17][17]);
+#endif //PLATEAU_H
