3cb235338787bfc33e88d31d0a0bba1d9aa2d6e4
Jeden971 <jeden.siloalexandrealexis@edu.ece.fr>
2024-11-21 13:08:36 +0100

    Update esthetique.h

--- esthetique.h
+++ esthetique.h
@@ -1,15 +1,42 @@
+#ifndef ESTHETIQUE_H
 #define ESTHETIQUE_H
-#ifndef AFFICHAGE_H
-#define AFFICHAGE_H
+
+#include <stdbool.h>
+
+// Définition des constantes pour le plateau
+#define MAX_PLAYERS 4
 #define CASE_VIDE '·'
 #define BARRIERE '|'
-#define BARRIERE_H'-'
-#define MAX_PLAYERS 4
+#define BARRIERE_H '-'
 
+// Propriétés des pions par défaut
+#define PION_J1 '@'
+#define PION_J2 '#'
+#define PION_J3 '$'
+#define PION_J4 '%'
 
-void esthetique(void);
-void esthetique2 (int i);
+// Structure pour représenter un joueur
+typedef struct {
+    int id;        // ID du joueur
+    char pion;     // Jeton choisi par le joueur
+} Joueur;
 
+// Prototypes des fonctions
 
-#endif ESTHETIQUE_H
-#endif ESTHETIQUE_H
+// Affichage des éléments esthétiques du plateau
+void esthetique();
+void esthetique2(int i);
+
+// Affiche les options disponibles pour choisir un pion
+void afficherOptionsPions();
+
+// Vérifie si un pion a déjà été choisi
+bool pionDejaUtilise(char pion, Joueur joueurs[], int nbJoueurs);
+
+// Permet à un joueur de choisir un pion
+void choisirPion(Joueur *joueur, Joueur joueurs[], int nbJoueurs);
+
+// Affiche le plateau avec les pions et le contenu personnalisé
+void afficherPlateau(int N, Joueur joueurs[], char tableau[17][17]);
+
+#endif // ESTHETIQUE_H
