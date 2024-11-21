f404b5521f7f8fcf9a084b6262fac586c2d74b03
damienbreneycollignon <damien.breneycollignon@edu.ece.fr>
2024-11-21 12:09:38 +0100

    Update timer.h

--- timer.h
+++ timer.h
@@ -1,1 +1,18 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+#include <time.h>
+
+// Définition de la structure pour le timer
+typedef struct {
+    time_t start_time;
+    int time_limit; // Durée limite en secondes
+} Timer;
+
+// Prototypes des fonctions
+void init_timer(Timer *timer, int time_limit);
+int get_elapsed_time(Timer *timer);
+int is_time_over(Timer *timer);
+
+#endif
 
