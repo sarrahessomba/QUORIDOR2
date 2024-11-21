9992c1eea81430ad2f5ca58821b3f045eaa7fb41
Jeden971 <jeden.siloalexandrealexis@edu.ece.fr>
2024-11-21 13:01:46 +0100

    Update esthetique.h

--- esthetique.h
+++ esthetique.h
@@ -5,10 +5,7 @@
 #define BARRIERE '|'
 #define BARRIERE_H'-'
 // utilisation de cractères unicode
-#define PION_J1 '⬤'     // Pion du joueur 1
-#define PION_J2 '○'     // Pion du joueur 2
-#define PION_J3 '◉'     // Pion du joueur 3 (seulement si 4 joueurs)
-#define PION_J4 '◎'     // Pion du joueur 4 (seulement si 4 joueurs)
+
 void esthetique(void);
 void esthetique2 (int i);
 
